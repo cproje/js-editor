d4491e60bfa074f72afe28241a666f78fc318ca4
Mehmet Doğan <66303486+cproje@users.noreply.github.com>
2021-04-26 20:54:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -30,15 +30,26 @@
     update();
     _input.focus();
   }
+
+
+  function insert(value, offset) {
+    let start = _input.selectionStart;
+    let end = _input.selectionEnd;
+    let first = _input.value.substring(0, start);
+    let last = _input.value.substring(end);
+    _input.value = first + value + last;
+    _input.selectionStart = start + offset;
+    _input.selectionEnd = start + offset;
+  }
   
   function update() {
     _output.innerHTML = _input.value
       .replace(/</g, '&lt;')
       .replace(/>/g, '&gt;')
-      .replace(/\b(await|break|case|catch|class|const|continue|debugger|default|delete|do|else|enum|export|extends|false|finally|for|function|if|implements|import|in|instanceof|interface|let|new|null|of|package|private|protected|public|return|super|switch|static|this|throw|try|True|typeof|var|void|while|with|yield)\b/g, '<span class="keyword">$1</span>')
+      .replace(/\b(await|break|case|catch|class|const|continue|debugger|default|delete|do|else|enum|export|extends|finally|for|function|if|implements|import|in|instanceof|interface|let|new|null|of|package|private|protected|public|return|super|switch|static|this|throw|try|typeof|var|void|while|with|yield)\b/g, '<span class="keyword">$1</span>')
       .replace(/\b(\w+)(?=\()\b/g, '<span class="struct">$1</span>')
       .replace(/\b(true|false)\b/g, '<span class="boolean">$1</span>')
-      .replace(/\b(\d+(\.\d+)?)\b/g, '<span class="number">$1</span>')
+      .replace(/\b(\d+(\.\d+)?|0x[0-9a-fA-F]+|0b[01]+)\b/g, '<span class="number">$1</span>')
       .replace(/('[^']*'|`[^`]*`)/g, (_ ,value) => {
         value = value.replace(/\//g, '&sol;');
         value = value.replace(/<[^>]+>/g, '');
@@ -66,15 +77,10 @@
       e.preventDefault();
     }
   });
+
   _input.addEventListener('keyup', (e) => {
     if (e.keyCode == 9) {
-      let start = _input.selectionStart;
-      let end = _input.selectionEnd;
-      let first = _input.value.substring(0, start);
-      let last = _input.value.substring(end);
-      _input.value = `${first}  ${last}`;
-      _input.selectionStart = start + 2;
-      _input.selectionEnd = start + 2;
+      insert('  ', 2);
     }
 
     if (e.keyCode == 121) {
